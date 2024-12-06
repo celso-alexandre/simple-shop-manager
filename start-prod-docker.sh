4e00c17875efb9b631960f31b725227972c76343
celso-alexandre <celsoalexandre@live.com>
2024-12-06 00:35:49 -0300

    feat: add start-prod-docker.sh script

---
+++ start-prod-docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd "$(dirname "$0")" || exit
+
+source ~/.bashrc
+
+git add .
+git stash
+git pull
+
+DOCKER_COMPOSE=$(command -v docker-compose || echo "docker compose")
+
+$DOCKER_COMPOSE down --remove-orphans
+$DOCKER_COMPOSE build
+$DOCKER_COMPOSE up -d
