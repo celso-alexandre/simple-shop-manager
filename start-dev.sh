a0c336980079634aaa39d39863a1c7bb1b35555c
Rosecler Palma <roseclermpalma1@gmail.com>
2024-12-05 22:15:39 -0300

    feat: added start-prod.sh script

--- start-dev.sh
+++ start-dev.sh
@@ -1,3 +1,2 @@
-nvm use 18
 concurrently "cd backend && pnpm i && pnpm start:dev" \
              "cd web && pnpm i && pnpm start:dev"
--- start-prod.sh
+++ start-prod.sh
@@ -1,4 +1,5 @@
 git add .
 git stash
+git pull
 concurrently "cd backend && pnpm i && pnpm build && pnpm start" \
              "cd web && pnpm i && pnpm build && pnpm preview"
