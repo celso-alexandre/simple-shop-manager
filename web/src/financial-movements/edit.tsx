de1d5549a982f905a8419acb4378ab7ddf906514
celso-alexandre <celsoalexandre@live.com>
2025-01-12 22:08:15 -0300

    feat: financial movements details pages: now shows caixa too

--- web/src/financial-movements/edit.tsx
+++ web/src/financial-movements/edit.tsx
@@ -12,8 +12,9 @@ import {
   FinancialMovementDocument,
   useFinancialMovementQuery,
 } from '../graphql/__generated__/financial-movements.gql.generated';
-import { serializeDecimalAsInt } from '../helpers';
+import { formatMoneyFromInt, serializeDecimalAsInt } from '../helpers';
 import { FinancialMovementsForm } from './form';
+import { useFinancialMovementAggregateQuery } from '../graphql/__generated__/financial-movement-aggregate.gql.generated';
 
 async function onSubmit(
   financialMovement: FinancialMovementsFormNode,
@@ -45,6 +46,7 @@ export function FinancialMovementEdit() {
       where: { id },
     },
   });
+  const { data: dataAggregate } = useFinancialMovementAggregateQuery();
 
   const initialValues = useMemo<FinancialMovementsFormNode | undefined>(() => {
     if (!data?.financialMovement) return undefined;
@@ -61,29 +63,49 @@ export function FinancialMovementEdit() {
 
   return (
     <>
-      <Title
-        title={data?.financialMovement?.id ?? 'Movimento não encontrado'}
-      />
+      <div className="flex h-full flex-col justify-between">
+        <div>
+          <Title
+            title={data?.financialMovement?.id ?? 'Movimento não encontrado'}
+          />
 
-      <FinancialMovementsForm
-        initialValues={initialValues}
-        form={form}
-        onFinish={async (values) => {
-          await form.validateFields();
-          onSubmit(values, update);
-        }}
-      />
+          <FinancialMovementsForm
+            initialValues={initialValues}
+            form={form}
+            onFinish={async (values) => {
+              await form.validateFields();
+              onSubmit(values, update);
+            }}
+          />
 
-      <Row style={{ marginTop: '20px' }}>
-        <Button
-          size="large"
-          type="primary"
-          onClick={() => {
-            form.submit();
-          }}>
-          Salvar
-        </Button>
-      </Row>
+          <Row style={{ marginTop: '20px' }}>
+            <Button
+              size="large"
+              type="primary"
+              onClick={() => {
+                form.submit();
+              }}>
+              Salvar
+            </Button>
+          </Row>
+        </div>
+
+        <div className="relative rounded-lg border border-gray-300 p-4">
+          <h3 className="absolute -top-3 left-4 bg-white px-2 text-sm font-semibold text-gray-700">
+            Caixa
+          </h3>
+          <div className="grid grid-cols-1 gap-x-28 sm:gap-x-12 lg:grid-cols-4">
+            <div className="flex justify-between">
+              <span className="font-semibold text-gray-700">Total</span>
+              <span className="text-gray-900">
+                {formatMoneyFromInt(
+                  dataAggregate?.financialMovementAggregate?.value
+                )}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- web/src/financial-movements/insert.tsx
+++ web/src/financial-movements/insert.tsx
@@ -9,7 +9,8 @@ import {
 } from '../graphql/__generated__/financial-movements.gql.generated';
 import { FinancialMovementsForm } from './form';
 import { FinancialMovementType } from '../types';
-import { serializeDecimalAsInt } from '../helpers';
+import { formatMoneyFromInt, serializeDecimalAsInt } from '../helpers';
+import { useFinancialMovementAggregateQuery } from '../graphql/__generated__/financial-movement-aggregate.gql.generated';
 
 async function onSubmit(
   data: FinancialMovementsFormNode,
@@ -34,29 +35,50 @@ export function FinancialMovementInsert() {
   const [create] = useCreateFinancialMovementMutation({
     refetchQueries: [FinancialMovementsDocument],
   });
+  const { data: dataAggregate } = useFinancialMovementAggregateQuery();
 
   return (
     <>
-      <Title title="Novo Movimento Financeiro" />
+      <div className="flex h-full flex-col justify-between">
+        <div>
+          <Title title="Novo Movimento Financeiro" />
 
-      <FinancialMovementsForm
-        form={form}
-        onFinish={async (values) => {
-          await onSubmit(values, create);
-        }}
-      />
+          <FinancialMovementsForm
+            form={form}
+            onFinish={async (values) => {
+              await onSubmit(values, create);
+            }}
+          />
 
-      <Row style={{ marginTop: '20px' }}>
-        <Button
-          size="large"
-          type="primary"
-          onClick={async () => {
-            await form.validateFields();
-            form.submit();
-          }}>
-          Salvar
-        </Button>
-      </Row>
+          <Row style={{ marginTop: '20px' }}>
+            <Button
+              size="large"
+              type="primary"
+              onClick={async () => {
+                await form.validateFields();
+                form.submit();
+              }}>
+              Salvar
+            </Button>
+          </Row>
+        </div>
+
+        <div className="relative rounded-lg border border-gray-300 p-4">
+          <h3 className="absolute -top-3 left-4 bg-white px-2 text-sm font-semibold text-gray-700">
+            Caixa
+          </h3>
+          <div className="grid grid-cols-1 gap-x-28 sm:gap-x-12 lg:grid-cols-4">
+            <div className="flex justify-between">
+              <span className="font-semibold text-gray-700">Total</span>
+              <span className="text-gray-900">
+                {formatMoneyFromInt(
+                  dataAggregate?.financialMovementAggregate?.value
+                )}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
